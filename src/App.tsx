726c708f64b86a9279b32ec8b7d0abeb2163b8bd
Guster <spavle.gh@gmail.com>
2024-01-02 18:16:02 +0100

    search bar, filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
+
+function Filters(){
+
+  const [ castList, setCastList ] = useState([]);
+  const [ director, setDirector ] = useState(null);
+
+  return(
+    <div className='filters-div'>
+      <div className='cast-div'>
+        <p>cast:</p>
+
+
+        <div className='name-div'>
+          <div className='name'>Akira Kurosawa</div>
+          <button className='remove-name-btn'> X</button>
+        </div>
+
+        <div className="addButton"></div>
+      </div>
+      <p>director:</p>
+      <p>year:</p>
+    </div>
+  );
+}
+
 function App() {
+
+  const [ filtersActive, setFiltersActive ] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='search-div'>
+        <div className='search-input-div'>
+          <input placeholder='Search' className='search-input'></input>
+          <button>
+            <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+          </button>
+        </div>
+
+        <button className='filters-btn' onClick={() => setFiltersActive(!filtersActive)}>Filters</button>
+      </div>
+
+      {(filtersActive)? <Filters/> : <></>}
+
+      <div className='movies-div'>
+
+      </div>
+
     </div>
   );
 }
